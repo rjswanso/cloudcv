013462351de29b000cd90895cb8f148fb7185300
rjswanso <rjswanso@gmail.com>
2024-01-05 15:01:17 -0500

    Updated visitor count script

--- Russell Swanson - Resume_files/visitor_count.js
+++ Russell Swanson - Resume_files/visitor_count.js
@@ -1,6 +1,7 @@
 const functionApi = 'https://ro7w9atei3.execute-api.us-east-1.amazonaws.com/cloudcvcounter';
             
-            if (window.location.href.indexOf('cloudfront') > -1 { const functionApi = 'https://cloudcvcounter.azurewebsites.net/api/HttpTrigger1?code=l1SRNhgPkbyCBuzrL5m-z3RGLZFdw1noC1ou6YPKgROPAzFu4Mt8UA=='; }
-            window.onload = fetch(azfunctionApi)
-                .then(response => response.json())
-                .then(response => {document.getElementById('counter').innerHTML = response;});
+if (window.location.href.indexOf('cloudfront') > -1 { const functionApi = 'https://cloudcvcounter.azurewebsites.net/api/HttpTrigger1?code=l1SRNhgPkbyCBuzrL5m-z3RGLZFdw1noC1ou6YPKgROPAzFu4Mt8UA=='; }
+window.onload = fetch(azfunctionApi)
+    .then(response => response.json())
+    .then(response => {document.getElementById('counter').innerHTML = response;});
+}
