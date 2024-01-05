c9f7f47bf72ae473f5c315a50762318801269044
rjswanso <rjswanso@gmail.com>
2024-01-05 12:39:44 -0500

    Updated visitor count script

--- Russell Swanson - Resume_files/visitors.js
+++
@@ -1,8 +0,0 @@
-
-    const url = 'https://xolsy4ysjg.execute-api.us-east-1.amazonaws.com/Prod/CountFunction'
-
-    window.onload = fetch(url)
-    .then(response => response.json())
-    .then(response => {
-      document.getElementById('count').innerHTML = response.visitors;
-    });
