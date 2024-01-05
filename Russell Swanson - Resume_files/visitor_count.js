d87f698d1023352573cbdcaba1bdcff7d4ee9471
rjswanso <rjswanso@gmail.com>
2024-01-05 16:30:51 -0500

    updated visitor count api

--- Russell Swanson - Resume_files/visitor_count.js
+++ Russell Swanson - Resume_files/visitor_count.js
@@ -1,6 +1,6 @@
-const functionApi = 'https://ro7w9atei3.execute-api.us-east-1.amazonaws.com/cloudcvcounter';
+const functionApi = 'https://cloudcvcounter.azurewebsites.net/api/HttpTrigger1?code=l1SRNhgPkbyCBuzrL5m-z3RGLZFdw1noC1ou6YPKgROPAzFu4Mt8UA==';
             
-if (window.location.href.indexOf('cloudfront') > -1) { const functionApi = 'https://cloudcvcounter.azurewebsites.net/api/HttpTrigger1?code=l1SRNhgPkbyCBuzrL5m-z3RGLZFdw1noC1ou6YPKgROPAzFu4Mt8UA=='; }
+if (window.location.href.indexOf('cloudfront') > -1) { const functionApi = 'https://ro7w9atei3.execute-api.us-east-1.amazonaws.com/cloudcvcounter'; }
 window.onload = fetch(functionApi)
     .then(response => response.json())
     .then(response => {document.getElementById('counter').innerHTML = response.visit_count;});
